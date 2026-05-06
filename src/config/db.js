bd9a2f61702006fab770997d6e7365bb6c6af434
Roushu01 <2023btechaidsroushni19776@poornima.edu.in>
2026-05-06 17:24:00 +0530

    fixed error

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,15 @@
 const pg=require("pg")
-
+require("dotenv").config();
 const Connect=async()=>{
     const client=new pg.Client({
         host:process.env.DB_HOST,
         port:5432,
         user:process.env.DB_USER,
         password:"9m2Q7hiqKT7tyfTfuxzKXejOVYI0Vdxm",
-        database:process.env.DB_NAME
+        database:process.env.DB_NAME,
+        ssl: {
+        rejectUnauthorized: false
+    }
     })
     await client.connect()
     console.log("Connected to PostgreSQL database")
