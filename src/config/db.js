71cec0cf68162b9e5bf03875cd65829066b41ff5
Roushu01 <2023btechaidsroushni19776@poornima.edu.in>
2026-05-06 16:48:26 +0530

    correct

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,12 @@
-const {Pool}=require("pg")
+const { Pool } = require("pg");
 
-const pool=new Pool({
-    connectionString: process.env.POSTGRES_URL,
-    ssl: {
-        rejectUnauthorized: false   
-    }
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
-module.exports=pool
+pool.connect()
+    .then(() => console.log("✅ DB connected"))
+    .catch(err => console.error("❌ DB error:", err));
+
+module.exports = pool;
